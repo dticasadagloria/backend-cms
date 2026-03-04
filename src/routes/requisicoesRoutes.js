2f1723ca457f692890a9b9a654b61ba61b512961
dticasadagloria <dticasadagloria@gmail.com>
2026-03-04 04:19:39 -0600

    cloudinary changes

--- src/routes/requisicoesRoutes.js
+++ src/routes/requisicoesRoutes.js
@@ -22,6 +22,7 @@ const storage = new CloudinaryStorage({
     folder:         "iicgp/comprovativos",
     allowed_formats: ["jpg", "jpeg", "png", "pdf"],
     resource_type:  "auto",
+    type:            "upload", 
   },
 });
 const upload = multer({ storage });
