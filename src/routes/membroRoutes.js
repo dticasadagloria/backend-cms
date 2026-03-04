ce8f67fbe88ad464630b1e70ebfbb1f8369a8102
dticasadagloria <dticasadagloria@gmail.com>
2026-03-04 00:26:12 -0600

    routes modified

--- src/routes/membroRoutes.js
+++ src/routes/membroRoutes.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import {
   getAllMembrosHandler,
   getMembroByIdHandler,
@@ -7,25 +7,35 @@ import {
   deleteMembroHandler,
   deleteMembroHardHandler,
   reactivateMembroHandler,
-  membrosSemCelula
-} from '../controllers/membroController.js';
-import { authenticate } from '../middleware/authMiddleware.js';
-import { requireRole } from '../middleware/authMiddleware.js';
+  membrosSemCelula,
+} from "../controllers/membroController.js";
+import { authenticate } from "../middleware/authMiddleware.js";
+import { requireRole } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // Todos os users autenticados podem ver
-router.get('/', authenticate, getAllMembrosHandler);
-router.get('/:id', authenticate, getMembroByIdHandler);
+router.get("/", authenticate, getAllMembrosHandler);
 router.get("/sem-celula", authenticate, membrosSemCelula);
+router.get("/:id", authenticate, getMembroByIdHandler);
 
 // Apenas Admin e Pastor podem criar/editar/deletar
-router.post  ('/',    authenticate, requireRole(1, 2), createMembroHandler);
-router.put   ('/:id', authenticate, requireRole(1, 2), updateMembroHandler);
-router.delete('/:id/hard', authenticate, requireRole(1), deleteMembroHardHandler);
+router.post("/", authenticate, requireRole(1, 2), createMembroHandler);
+router.put("/:id", authenticate, requireRole(1, 2), updateMembroHandler);
+router.delete(
+  "/:id/hard",
+  authenticate,
+  requireRole(1),
+  deleteMembroHardHandler,
+);
 
 //Desativar/Ativar o estado do membro para ativo ou inativo
-router.delete('/:id', authenticate, requireRole(1), deleteMembroHandler);
-router.patch('/:id/reactivate', authenticate, requireRole(1), reactivateMembroHandler);
+router.delete("/:id", authenticate, requireRole(1), deleteMembroHandler);
+router.patch(
+  "/:id/reactivate",
+  authenticate,
+  requireRole(1),
+  reactivateMembroHandler,
+);
 
 export default router;
